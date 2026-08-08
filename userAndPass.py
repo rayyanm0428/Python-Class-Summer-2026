f9a9b8e3ea95d83ab667d33b686a066ec46d76c8
rayyan0428 <gweenapoles@gmail.com>
2026-08-08 15:39:09 -0400

    adding userAndPass.py

---
+++ userAndPass.py
@@ -0,0 +1,11 @@
+a = str(input("Enter username: "))
+if a != "rayyan0428":
+    print("This user does not exist")
+else:
+    b = str(input("Enter password: "))
+
+if a == "rayyan0428" and b == "password":
+    print("Login successful")
+    print("Welcome", a)
+elif a == "rayyan0428" and b != "password":
+    print("Incorrect password, please try again")
